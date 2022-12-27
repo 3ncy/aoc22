f6d9dd7abf26c6b995902776d466506aec3b1dfa
encyk <martiiin225@email.cz>
2022-12-27 19:10:58 +0100

    day 21 pt 2 finally! - operand order is hard xD

--- 21/21-2.js
+++ 21/21-2.js
@@ -1,59 +1,87 @@
 const fs = require('fs');
-let monkeys = fs.readFileSync('in.test.txt').toString().split("\n").map(l => {
+let monkeys = fs.readFileSync('in.txt').toString().split("\n").map(l => {
     l = l.split(" ");
     let monkey = {};
     monkey.name = l[0].substring(0, l[0].length - 1);
     if (isNaN(l[1])) {  // is a computing monkey        
         monkey.m1 = l[1];
-        // monkey.operation = l[2];
-        switch (l[2]) { //micro optimalization, likely useless, but I gotta try
-            case "+":
-                monkey.operation = 0;
-                break;
-            case "-":
-                monkey.operation = 1;
-                break;
-            case "*":
-                monkey.operation = 2;
-                break;
-            case "/":
-                monkey.operation = 3;
-                break;
-        }
+        monkey.operation = l[2];
         monkey.m2 = l[3];
 
         if (monkey.name == "root")
             monkey.operation = " === "
+
     } else { //is a constant monkey
         monkey.number = Number(l[1])
     }
+    monkey.hasHumn = false;
     return monkey;
 });
 
-// console.log(monkeys);
-//process.argv
-let humn = 0;
-while (true) {
-    if (humn % 200 == 0) console.log(humn)
-    monkeys.find(x => x.name == "humn").number = humn;
-    if (getMonkeyValue("root") === true) {
-        console.log("humn number is: " + humn);
-        break;
-    } else {
-        humn++;
+let equalsVal = getMonkeyValue("root");
+
+console.log(equalsVal);
+calculateFor("root", equalsVal)
+
+
+function calculateFor(monkeyName, value) {
+    let monkey = monkeys.find(m => m.name == monkeyName)
+
+    console.log("calculating for " + monkeyName + " with the value " + value + " and op " + monkey.operation);
+
+    let m1 = monkeys.find(m => m.name == monkey.m1);
+    let m2 = monkeys.find(m => m.name == monkey.m2);
+
+    if (monkeyName === "root") {
+        //for this maybe I could use something like PHP does "|| die()"
+        if (m1.hasHumn)
+            calculateFor(monkey.m1, value);
+        else
+            calculateFor(monkey.m2, value);
+        return;
     }
+
+    if (monkeyName === "humn") {
+        console.log("humn's value is " + value);
+        return;
+    }
+
+    switch (monkey.operation) {
+        case "+":
+            if (m1.hasHumn)
+                calculateFor(m1.name, value - m2.number)
+            else
+                calculateFor(m2.name, value - m1.number);
+            break;
+        case "-": // val = m1 - m2 => 15 = 20 - 5
+            if (m1.hasHumn)
+                calculateFor(m1.name, value + m2.number); // 20 = 15 + 5 => m1 = val + m2
+            else
+                calculateFor(m2.name, m1.number - value); // 5 = 20 - 15 => m2 = m1 - val 
+            break;
+        case "/": // val = m1 / m2  =>  3 = 15 / 5
+            if (m1.hasHumn)
+                calculateFor(m1.name, value * m2.number); // 15 = 3 * 5    =>   m1 = val * m2
+            else
+                calculateFor(m2.name, m1.number / value); // 5 = 15 / 3    =>   m2 = m1 / val
+            break;
+        case "*":
+            if (m1.hasHumn)
+                calculateFor(m1.name, value / m2.number);
+            else
+                calculateFor(m2.name, value / m1.number);
+            break;
+    }
+
 }
-
-
-
-// console.log("Value of drzm is " + getMonkeyValue("drzm"));
-// console.log("Value of sjmn is " + getMonkeyValue("sjmn"));
-// console.log("Value of lfqf is " + getMonkeyValue("lfqf"));
-// console.log("Value of root is " + getMonkeyValue("root"));
-
 
 function getMonkeyValue(monkeyName) {
     let monkey = monkeys.find(m => m.name == monkeyName)
+
+    if (monkey.name === "humn") {
+        monkey.hasHumn = true;
+        return null;
+    }
 
     if (typeof monkey.number !== "undefined") {
         return monkey.number;
@@ -63,23 +91,22 @@ function getMonkeyValue(monkeyName) {
     let m2 = getMonkeyValue(monkey.m2);
 
     if (monkey.name == "root") {
-        return m1 === m2;
+        //return the valid branch's value
+        console.log({ m1: m1, m2: m2 })
+        if (m1 === null) {
+            return m2;
+        } else {
+            return m1;
+        }
     }
 
-    switch (monkey.operation) {
-        case 0:
-            return m1 + m2; //idk, the conversion maybe isn't neccessary, but I ain't trust js
-            break;
-        case 1:
-            return m1 - m2;
-            break;
-        case 2:
-            return m1 * m2;
-            break;
-        case 3:
-            return m1 / m2;
-            break;
+    //if this branch includes the humn, return
+    if (m1 === null || m2 === null) {
+        monkey.hasHumn = true;
+        return null;
     }
 
-    // return Number(eval(m1 + "" + monkey.operation + "" + m2));
+    let result = Number(eval(m1 + "" + monkey.operation + "" + m2))
+    monkey.number = result;
+    return result;
 }
